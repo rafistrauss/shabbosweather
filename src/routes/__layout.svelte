f26e6b9489e10dd507f1859fcb302b988f3c3174
Rafi Strauss <rafikis75@gmail.com>
2021-05-12 00:28:43 -0400

    Fix paths for icons

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,11 +5,11 @@
 <svelte:head>
 	<title>ShabbosWeather 2.0</title>
 	<link rel="stylesheet" href="weather-icons.min.css" />
-	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-	<link rel="manifest" href="/site.webmanifest" />
-	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+	<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+	<link rel="manifest" href="site.webmanifest" />
+	<link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5" />
 	<meta name="msapplication-TileColor" content="#b91d47" />
 	<meta name="theme-color" content="#ffffff" />
 </svelte:head>
