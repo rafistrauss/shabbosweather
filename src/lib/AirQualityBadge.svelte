dec15e8c2c59f6c1f809655e8985736ee4a6d45a
Rafi Strauss <rafi.strauss@nbcuni.com>
2023-06-30 16:02:44 -0400

    refactor: pull air quality badge into own component

---
+++ src/lib/AirQualityBadge.svelte
@@ -0,0 +1,41 @@
+<script>
+	export let aqi;
+	const airQualityText = {
+		1: 'Good',
+		2: 'Fair',
+		3: 'Moderate',
+		4: 'Poor',
+		5: 'Very Poor'
+	};
+
+	const airQualityBadgeColor = {
+		1: 'green',
+		2: 'yellow',
+		3: 'orange',
+		4: 'red',
+		5: 'purple'
+	};
+
+	const airQualityTextColor = {
+		1: 'whitesmoke',
+		2: '#2a2a2a',
+		3: '#2a2a2a',
+		4: 'whitesmoke',
+		5: 'whitesmoke'
+	};
+</script>
+
+<span
+	id="airQuality"
+	style="color: {airQualityTextColor[aqi]};background-color: {airQualityBadgeColor[aqi]};"
+>
+	{airQualityText[aqi]}
+</span>
+
+<style>
+	#airQuality {
+		padding: 0.5em;
+		border-radius: 5px;
+		margin: 1em 0;
+	}
+</style>
--- src/lib/CurrentWeather.svelte
+++ src/lib/CurrentWeather.svelte
@@ -1,27 +1,12 @@
 <script>
 	import { lightFormat } from 'date-fns';
+	import AirQualityBadge from './AirQualityBadge.svelte';
 
 	/**
 	 * @type {current}
 	 */
 	export let current;
 	const { feels_like, dt: timestamp, weather, humidity, wind_speed, aqi } = current;
-
-	const airQualityText = {
-		1: 'Good',
-		2: 'Fair',
-		3: 'Moderate',
-		4: 'Poor',
-		5: 'Very Poor'
-	};
-
-	const airQualityColor = {
-		1: 'green',
-		2: 'yellow',
-		3: 'orange',
-		4: 'red',
-		5: 'purple'
-	};
 
 	const { icon, id } = weather[0];
 
@@ -44,17 +29,6 @@
 		<br />
 		<!-- Display air quality color based on the quality -->
 		Air Quality:
-		<span id="airQuality" style="background-color: {airQualityColor[aqi]};">
-			{airQualityText[aqi]}
-		</span>
+		<AirQualityBadge {aqi} />
 	</p>
 </div>
-
-<style>
-	#airQuality {
-		color: gainsboro;
-		padding: 0.5em;
-		border-radius: 5px;
-		margin: 1em 0;
-	}
-</style>
