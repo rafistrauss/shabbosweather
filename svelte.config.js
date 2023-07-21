65d7d62a885fb507d0d37b57463b5949476f4832
Rafi Strauss <rafi.strauss@nbcuni.com>
2023-07-21 00:49:48 -0400

    ci: Fix github pages (#10)

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/shabbosweather'
+		}
+	}
+};
