43bc0e1333ecf097eea723cbef850675a2cc8724
rafistrauss <rafistrauss@users.noreply.github.com>
2024-10-27 05:33:42 +0000

    deploy: 989bc10a96a2bee214731522a90daacdd22760b6

--- _app/immutable/chunks/paths.1348e703.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_6sn0xe)==null?void 0:s.base)??"/shabbosweather";var e;const t=((e=globalThis.__sveltekit_6sn0xe)==null?void 0:e.assets)??a;export{t as a,a as b};
